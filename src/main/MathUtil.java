f8ba49e7f907fbf35c744f50d04a94a855b02dbc
unknown <meicy101@gmail.com>
2020-10-12 17:29:52 -0500

    second

--- src/main/MathUtil.java
+++ src/main/MathUtil.java
@@ -4,8 +4,22 @@ public class MathUtil {
 	public int add(int a, int b) {
 		return a+b;
 	}
-	public boolean isOdd (int a) {
-		if(a %2!=0) return true;
-		return false;
+//	public boolean isOdd (int a) {
+//		if(a %2!=0) return true;
+//		return false;
+//	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	public int div(int a, int b) {
+		return a/b;
+	}
+	public int mult(int a, int b) {
+		return a*b;
+	}
+	
+	public double circ(double d) {
+		return 2*d*Math.PI;
 	}
 }
